b1ffaab001ffa050d8c07774ae12cd7443d5b82e
amalianurf <amalia19006@mail.unpad.ac.id>
2025-02-04 19:54:10 +0700

    Fix package-lock.json and navbar

--- Readme.txt
+++
@@ -1,1 +0,0 @@
-Laravel marketplace project
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -75,6 +75,10 @@
                 <span class="absolute bottom-0 left-0 w-full h-0.5 bg-primary-dark scale-x-0 group-hover:scale-x-100 transition-transform origin-left"></span>
             </x-nav-fashion>
 
+            <x-nav-rumah class="group relative text-secondary hover:text-primary-dark px-3 py-2 text-sm font-medium transition-all duration-300">
+                <span class="absolute bottom-0 left-0 w-full h-0.5 bg-primary-dark scale-x-0 group-hover:scale-x-100 transition-transform origin-left"></span>
+            </x-nav-rumah>
+
              <a href="{{ route('umkm') }}" class="{{ request()->is('UMKM') ? 'text-primary-dark font-bold' : 'text-secondary' }} px-3 py-2 text-sm font-medium transition-all duration-300 hover:text-primary-dark">
                 Produk Diskon
                 <span class="absolute bottom-0 left-0 w-full h-0.5 bg-primary-dark scale-x-0 group-hover:scale-x-100 transition-transform origin-left"></span> --}}
