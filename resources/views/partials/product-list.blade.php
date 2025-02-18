e7f8d3b7c68fdbb215a2a42e7e96042e18432e6e
amalianurf <amalia19006@mail.unpad.ac.id>
2025-02-18 19:39:37 +0700

    Fix search logic controller

--- resources/views/partials/product-list.blade.php
+++ resources/views/partials/product-list.blade.php
@@ -1,5 +1,5 @@
 @if(isset($products) && $products->isEmpty())
-    <p class="text-center text-secondary">Produk tidak ditemukan.</p>
+    <p class="absolute text-center text-secondary">Produk tidak ditemukan.</p>
 @else
     @foreach ($products as $product)
         <x-product-card :product="$product" />
