027cc844a8144a367ac5135034fe957298915173
mazufa14 <maulanahusen848@gmail.com>
2025-03-17 18:31:54 +0700

    create crud(delete) in admin products controllers

--- app/Http/Controllers/AdminControllers.php
+++ app/Http/Controllers/AdminControllers.php
@@ -3,6 +3,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\QueryException;
 
 class AdminControllers extends Controller {
 	public function index() {
@@ -13,5 +16,20 @@ class AdminControllers extends Controller {
 	public function addProduct()
 	{
 		return view('admin.product-form');
+	}
+
+	
+	public function destroy(string $id)
+	{
+		try {
+			$product = Product::findOrFail($id);
+			$product->delete();
+			
+			return redirect()->route('admin.dashboard')->with('success', 'Data berhasil dihapus');
+		} catch (QueryException $e) {
+			return redirect()->route('admin.dashboard')->with('error', 'Gagal menghapus data. Terdapat relasi yang terkait.');
+		} catch (\Exception $e) {
+			return redirect()->route('admin.dashboard')->with('error', 'Terjadi kesalahan saat menghapus data.');
+		}
 	}
 }
