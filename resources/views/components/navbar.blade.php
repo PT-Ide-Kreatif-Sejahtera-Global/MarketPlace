ade37ae16a84044234c519a2ed41f6921aa5d577
amalianurf <amalia19006@mail.unpad.ac.id>
2025-03-18 22:28:24 +0700

    Added button login admin

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -34,12 +34,20 @@
             </div>
 
 			<div class="hidden md:flex items-center space-x-4">
-				<a href="https://ideathings.id/" class="flex items-center space-x-2 px-2 lg:px-4 py-2 rounded-full bg-primary text-secondary-dark font-bold transition-all duration-300 hover:shadow-lg hover:scale-105 group">
-					<svg class="h-5 w-5 transition-transform group-hover:rotate-12" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" />
-					</svg>
-					<span class="md:hidden lg:block">iDeaThings</span>
-				</a>
+                @guest
+                    <a href="{{ route('login') }}" class="flex items-center space-x-2 px-2 lg:px-4 py-2 rounded-full bg-primary text-secondary-dark font-bold transition-all duration-300 hover:shadow-lg hover:scale-105 group">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 transition-transform group-hover:rotate-12" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M480-120v-80h280v-560H480v-80h280q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H480Zm-80-160-55-58 102-102H120v-80h327L345-622l55-58 200 200-200 200Z"/></svg>
+                        <span class="md:hidden lg:block">Login Admin</span>
+                    </a>
+                @endguest
+                @auth
+                    <a href="{{ route('admin.dashboard') }}" class="flex items-center space-x-2 px-2 lg:px-4 py-2 rounded-full bg-primary text-secondary-dark font-bold transition-all duration-300 hover:shadow-lg hover:scale-105 group">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 transition-transform group-hover:rotate-12" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor">
+                            <path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z"/>
+                        </svg>
+                        <span class="md:hidden lg:block">Admin</span>
+                    </a>
+                @endauth
 			</div>
 
             <!-- Mobile Menu Toggle -->
